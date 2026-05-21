2971af10a0f01e4aabc7c6a090926cd77fed42da
Rumen Simov <Rumen_Simov@etu.u-bourgogne.fr>
2026-05-21 14:45:32 +0200

    Partie 2 : CRUD des ingrédients - Formulaire pour créer un ingrédient (construction, rendu et traitement

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Ingredient;
+use App\Form\IngredientType;
 use App\Repository\IngredientRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,6 +25,27 @@ final class IngredientController extends AbstractController
 
         return $this->render('pages/ingredient/index.html.twig', [
             'ingredients' => $ingredients,
+        ]);
+    }
+
+    #[Route('/ingredient/nouveau', name: 'ingredient_new', methods: ['GET', 'POST'])]
+    public function new(EntityManagerInterface $manager, Request $request): Response
+    {
+        $ingredient = new Ingredient();
+        $form = $this->createForm(IngredientType::class, $ingredient);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $ingredient = $form->getData();
+            $manager->persist($ingredient);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_ingredient');
+        }
+
+        return $this->render('pages/ingredient/new.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
