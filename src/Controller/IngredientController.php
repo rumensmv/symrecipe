ccfff395e172f4bd5f249417925e8f697a440b92
Rumen Simov <Rumen_Simov@etu.u-bourgogne.fr>
2026-05-21 14:12:25 +0200

    Partie 2 : CRUD des ingrédients - Liste des ingrédients

---
+++ src/Controller/IngredientController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\IngredientRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class IngredientController extends AbstractController
+{
+    #[Route('/ingredient', name: 'app_ingredient')]
+    public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
+    {
+        $ingredients = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        return $this->render('pages/ingredient/index.html.twig', [
+            'ingredients' => $ingredients,
+        ]);
+    }
+}
